1ad17cb4fab45cedf941292f01a8858398172a9c
Samuel Slade <12849513+sladesamuel@users.noreply.github.com>
2023-06-13 23:44:05 +0100

    Added listener to forward ALB traffic to ASG via Target Group

--- terraform/load-balancing.tf
+++ terraform/load-balancing.tf
@@ -33,14 +33,32 @@ resource "aws_lb" "app" {
     module.network.app_subnets.subnet_b
   ]
 
+
+
   tags = {
     Name = "${local.prefix}-alb"
   }
 }
 
 resource "aws_lb_target_group" "app_servers" {
-  name     = "${local.prefix}-alb-target-app-servers"
+  name = "${local.prefix}-alb-target-app-servers"
+
   port     = 80
   protocol = "HTTP"
   vpc_id   = module.network.vpc_id
+}
+
+resource "aws_lb_listener" "app_servers" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = aws_lb_target_group.app_servers.port
+  protocol          = aws_lb_target_group.app_servers.protocol
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_servers.arn
+  }
+
+  tags = {
+    Name = "${local.prefix}-alb-listener-app-servers"
+  }
 }
