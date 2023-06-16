bc641a8a30ade7e0b113a63bf898335095993da1
Samuel Slade <12849513+sladesamuel@users.noreply.github.com>
2023-06-16 21:38:27 +0100

    Allow HTTPS ingress on instance

--- transit-gateway/terraform/modules/private-networked-instance/instance.tf
+++ transit-gateway/terraform/modules/private-networked-instance/instance.tf
@@ -3,25 +3,42 @@ resource "aws_security_group" "instance" {
   description = "Allows inbound ICMP traffic to the EC2 Instance and all outbound traffic"
   vpc_id      = aws_vpc.network.id
 
-  ingress {
-    description = "Allow inbound ICMP traffic"
-    cidr_blocks = ["0.0.0.0/0"]
-    protocol    = "icmp"
-    from_port   = 0
-    to_port     = 0
-  }
-
-  egress {
-    description = "Allow all outbound traffic"
-    cidr_blocks = ["0.0.0.0/0"]
-    protocol    = -1
-    from_port   = 0
-    to_port     = 0
-  }
+  ingress = []
+  egress  = []
 
   tags = {
     Name = "${var.name}-sg"
   }
+}
+
+resource "aws_security_group_rule" "inbound_icmp" {
+  security_group_id = aws_security_group.instance.id
+  description       = "Allow inbound ICMP traffic"
+  type              = "ingress"
+  protocol          = "icmp"
+  from_port         = 0
+  to_port           = 0
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "inbound_https" {
+  security_group_id = aws_security_group.instance.id
+  description       = "Allow inbound HTTPS traffic"
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "outbound_all" {
+  security_group_id = aws_security_group.instance.id
+  description       = "Allow all outbound traffic"
+  type              = "egress"
+  protocol          = -1
+  from_port         = 0
+  to_port           = 0
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 resource "aws_instance" "instance" {
